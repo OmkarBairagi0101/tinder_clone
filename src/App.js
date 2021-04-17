13b67c87f7bd4f61db6668c5f816df4e95f3a5d4
omkarbairagi0101 <omkarbairagi0101@gmail.com>
2021-04-17 10:33:27 +0530

    initialized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEN class naming convention
+    <div className="app">
+      <h1>MERN Tinder-Clone</h1>
+    </div>
+  );
+}
+
+export default App;
